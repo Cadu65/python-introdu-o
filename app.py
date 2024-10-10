6568dc5fc50bf47b68168400479c29575c97ae99
Cadu65 <carlinhos2jeeh@gmail.com>
2024-10-10 11:33:35 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,1 @@
-nome_completo = input("Digite seu nome completo: ")
-
-Idade = int(input("Digite sua idade: "))
-
-print("Seu nome completo é:", nome_completo)
-print("Sua idade é:" ,idade, "anos")
+Numero1
